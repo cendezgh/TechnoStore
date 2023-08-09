45f6d2d999df882ebe845d2b47330dced4b3d6d6
Cesar Hernandez <cendez@hotmail.com>
2023-08-08 22:24:34 -0500

    Primer commit

---
+++ src/components/CartWidget.jsx
@@ -0,0 +1,8 @@
+import carrito from "../assets/cart-icon.webp";
+
+export const CartWidget = () => (
+  <>
+    <img src={carrito} alt="Cart" />
+    <span>23</span>
+  </>
+);
---
+++ src/components/NavNar.jsx
@@ -0,0 +1,18 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { CartWidget } from "./CartWidget";
+
+export const NavBar = () => (
+  <Navbar bg="light" data-bs-theme="light">
+    <Container>
+      <Navbar.Brand href="#home">TechnoStore</Navbar.Brand>
+      <Nav className="me-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#features">Features</Nav.Link>
+        <Nav.Link href="#pricing">Pricing</Nav.Link>
+      </Nav>
+      <CartWidget />
+    </Container>
+  </Navbar>
+);

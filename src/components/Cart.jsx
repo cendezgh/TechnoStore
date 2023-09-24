693720e36109888f7acd98307d0b41f285d93c6e
caryces <cendez@hotmail.com>
2023-09-24 12:42:03 -0500

    funcionalidad carrito y envio  a firebase compra

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,46 +1,118 @@
 import { Container, Table } from "react-bootstrap";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { CartContext } from "../contexts/CartContext";
+import { Form, Button } from "react-bootstrap";
+import { getFirestore, collection, addDoc } from "firebase/firestore";
 
 export const Cart = () => {
+  const [formValues, setFormValues] = useState({
+    name: "",
+    phone: "",
+    email: "",
+  });
+
   const { items, removeItem } = useContext(CartContext);
 
-  const total = () => 
-    items.reduce((acumulador, valorActual) => acumulador + valorActual.quantity * valorActual.precio, 0);
+  const total = () =>
+    items.reduce(
+      (acumulador, valorActual) =>
+        acumulador + valorActual.quantity * valorActual.precio,
+      0
+    );
+
+  const handleChange = ev => {
+    setFormValues(prev => ({
+      ...prev, [ev.target.name]: ev.target.value
+    }))
+  };
+
+  const sendOrder = () => {
+    const order = {
+      buyer: formValues,
+      items,
+      total: total(),
+    };
+
+    const db = getFirestore();
+    const orderCollection = collection(db, "orders");
+
+    addDoc(orderCollection, order).then(({ id }) => {
+      if (id) {
+        alert("Su order: " + id + " ha sido creada!");
+      }
+    });
+  };
   return (
     <Container>
       <h1>Cart</h1>
       <Table striped bordered hover variant="dark">
         <thead>
-            <tr>
-                <th>Nombre</th>
-                <th>Precio</th>
-                <th>Cantidad</th>
-                <th></th>
-            </tr>
+          <tr>
+            <th>Nombre</th>
+            <th>Precio</th>
+            <th>Cantidad</th>
+            <th></th>
+          </tr>
         </thead>
         <tbody>
-            {items.map(item => (
-                <tr key={item.id}>
-                    <td>{item.categoryId} {item.marca}</td>
-                    <td>{item.precio}</td>
-                    <td>{item.quantity}</td>
-                    <td>
-                        <button onClick={() => removeItem(item.id)}>Eliminar</button>
-                    </td>
-                </tr>
-            ))}
+          {items.map((item) => (
+            <tr key={item.id}>
+              <td>
+                {item.categoryId} {item.marca}
+              </td>
+              <td>{item.precio}</td>
+              <td>{item.quantity}</td>
+              <td>
+                <button onClick={() => removeItem(item.id)}>Eliminar</button>
+              </td>
+            </tr>
+          ))}
         </tbody>
         <tfoot>
-            <tr>
-                <td>Total</td>
-                <td></td>
-                <td></td>
-                <td>{total()}</td>
-            </tr>
+          <tr>
+            <td>Total</td>
+            <td></td>
+            <td></td>
+            <td>{total()}</td>
+          </tr>
         </tfoot>
-
       </Table>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Nombre</Form.Label>
+          <Form.Control
+            onChange={handleChange}
+            value={formValues.name}
+            type="text"
+            name="name"
+            placeholder="Nombre"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            onChange={handleChange}
+            value={formValues.email}
+            type="email"
+            name="email"
+            placeholder="name@example.com"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Teléfono</Form.Label>
+          <Form.Control
+            onChange={handleChange}
+            value={formValues.phone}
+            type="text"
+            name="phone"
+            placeholder="000 000 0000"
+          />
+        </Form.Group>
+        <Button variant="secondary" type="button" onClick={sendOrder}>
+          Comprar
+        </Button>
+      </Form>
+      {/* <button onClick={sendOrder}>Comprar</button> */}
     </Container>
   );
 };
